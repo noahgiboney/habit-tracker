3fa5b1471a8640116fe928c3e725fdf2b5da8bc7
noahgiboney <noahgiboney@gmail.com>
2023-12-24 16:41:39 -0800

    feat: edit button to delete habits from home view

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -9,41 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var addHabit = false
+    @State private var showDeleteHabit = false
     @State private var habits = Habits()
     
     var body: some View {
         NavigationStack{
             Form{
                 ForEach(habits.userHabits){ habit in
-                    NavigationLink(value: habit, label: {
-                        HStack{
-                            Text(habit.name)
-                            Text("\(habit.count)")
-                        }
-                    })
-                    .padding()
-                    
-                    Button("Increment"){
-                        //increase count for habit here
+                    Section{
+                        NavigationLink(value: habit, label: {
+                            HStack{
+                                Text(habit.name)
+                                Text("\(habit.count)")
+                            }
+                        })
+                        .padding()
                     }
                 }
+                .onDelete(perform:{ indexSet in
+                    delete(at: indexSet)
+                })
+
             }
             .navigationTitle("Habit Tracker")
             .navigationDestination(for: Habit.self){ habit in
                 Text("\(habit.id)")
             }
             .toolbar{
-                ToolbarItem{
+                ToolbarItem(placement: .topBarTrailing){
                     Button{
                         addHabit.toggle()
-                    } label: {
-                        Image(systemName: "plus")
+                    }label: {
+                        Text("Add")
                     }
+                }
+                ToolbarItem(placement: .topBarLeading){
+                    EditButton()
                 }
             }
         }.sheet(isPresented: $addHabit, content: {
             AddHabitView(habits: habits)
         })
+    }
+    func delete(at offsets: IndexSet){
+        habits.userHabits.remove(atOffsets: offsets)
     }
 }
 
