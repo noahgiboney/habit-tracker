3bf02524dda171130c27b29662ace520401a9441
noahgiboney <noahgiboney@gmail.com>
2024-01-02 12:52:54 -0800

    feat: new sheet for adding habits, also added a new date format

---
+++ HabitTracker/Views/AddEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEntryView.swift
+//  HabitTracker
+//
+//  Created by Noah Giboney on 1/2/24.
+//
+
+import SwiftUI
+
+struct AddEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEntryView()
+}
