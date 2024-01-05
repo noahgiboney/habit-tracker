fc4fdb6db71da480702e335286a3dd26f06cfe26
noahgiboney <noahgiboney@gmail.com>
2024-01-04 18:47:16 -0800

    chore: cleaned up code

--- HabitTracker/Views/AddHabitView.swift
+++ HabitTracker/Views/AddHabitView.swift
@@ -8,6 +8,7 @@ import SwiftData
 import SwiftUI
 
 struct AddHabitView: View {
+    
     @Environment(\.modelContext) var context
     @Environment(\.dismiss) var dismiss
     
@@ -15,15 +16,11 @@ struct AddHabitView: View {
     @State private var type = Habit.habitTypes[0]
     
     var body: some View {
-        
         NavigationStack{
-            
             Form{
-                
                 Section{
-                    
                     TextField("Habit Name", text: $name)
-                    
+                
                     Picker("Habit Type", selection: $type){
                         ForEach(Habit.habitTypes, id: \.self) {
                             Text($0)
@@ -32,15 +29,8 @@ struct AddHabitView: View {
                     .pickerStyle(.segmented)
                 }
                 
-                Section{
-                    
-                    Button("Save"){
-                        let newHabit = Habit(name: name, type: type)
-                        context.insert(newHabit)
-                        dismiss()
-                    }
+                Button("Save", action: saveHabit)
                     .disabled(Habit.validTextInput(input: name))
-                }
             }
             .navigationTitle("Add Habit")
             .navigationBarTitleDisplayMode(.inline)
@@ -49,10 +39,15 @@ struct AddHabitView: View {
                     Button("Cancel"){
                         dismiss()
                     }
-                    .foregroundColor(.red)
                 }
             }
         }
+    }
+    
+    func saveHabit(){
+        let newHabit = Habit(name: name, type: type)
+        context.insert(newHabit)
+        dismiss()
     }
 }
 
